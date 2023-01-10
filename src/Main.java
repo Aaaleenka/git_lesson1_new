c27cbc602186dce0d187e4440aa15726aebed2c7
Aaaleenka <missis.komi@gmail.com>
2023-01-10 20:03:43 +0300

    Изменен Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int[] sales = {500, 3000, 3100, 5300, 5400, 4200 };
+        SalesManager manager = new SalesManager(sales);
+        System.out.println("Лучшая продажа у менеджера = " + manager.max());
     }
 }
